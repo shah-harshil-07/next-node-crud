878efb90106d8cd67700a01a7fc4c5298a9d49ab
Harshil Shah <harshil.shah@bytestechnolab.com>
2022-09-28 06:31:07 +0530

    Worked on Login form UI - [WIP]

--- pages/login.js
+++ pages/login.js
@@ -1,51 +1,67 @@
+// import '../styles/Login.module.css';
+import styles from '../styles/Login.module.css';
+
 const login = () => {
     return (
-        <body class="img js-fullheight">
-            <section class="ftco-section">
-                <div class="container">
-                    <div class="row justify-content-center">
-                        <div class="col-md-6 text-center mb-5">
-                            <h2 class="heading-section">Login #10</h2>
+        <div className={styles['login-wrap']}>
+            <div className={styles['login-html']}>
+
+                <input id="tab-1" type="radio" name="tab" className={styles['sign-in']} defaultChecked />
+                <label htmlFor="tab-1" className={styles['tab']}><center>Sign In</center></label>
+
+                <input id="tab-2" type="radio" name="tab" className={styles['sign-up']} />
+                <label htmlFor="tab-2" className={styles['tab']}>Sign Up</label>
+
+                <div className={styles['login-form']}>
+                    <div className={styles['sign-in-htm']}>
+                        <div className={styles['group']}>
+                            <span className={styles['p-float-label']}>
+                                {/* <InputText
+                                    id='login-user'
+                                    value={username}
+                                    className="input"
+                                    onChange={e => setUsername(e.target.value)}
+                                /> */}
+                                <input className={styles['input']} type="text" />
+                                <label htmlFor="user" style={{ paddingLeft: '20px' }} className={styles['label']}>Username</label>
+                            </span>
                         </div>
-                    </div>
-                    <div class="row justify-content-center">
-                        <div class="col-md-6 col-lg-4">
-                            <div class="login-wrap p-0">
-                                <h3 class="mb-4 text-center">Have an account?</h3>
-                                <form action="#" class="signin-form">
-                                    <div class="form-group">
-                                        <input type="text" class="form-control" placeholder="Username" required="" />
-                                    </div>
-                                    <div class="form-group">
-                                        <input id="password-field" type="password" class="form-control" placeholder="Password" required="" />
-                                            <span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
-                                    </div>
-                                    <div class="form-group">
-                                        <button type="submit" class="form-control btn btn-primary submit px-3">Sign In</button>
-                                    </div>
-                                    <div class="form-group d-md-flex">
-                                        <div class="w-50">
-                                            <label class="checkbox-wrap checkbox-primary">Remember Me
-                                                <input type="checkbox" checked="" />
-                                                <span class="checkmark"></span>
-                                            </label>
-                                        </div>
-                                        <div class="w-50 text-md-right">
-                                            <a href="#" style={{color: '#fff'}}>Forgot Password</a>
-                                        </div>
-                                    </div>
-                                </form>
-                                <p class="w-100 text-center">— Or Sign In With —</p>
-                                <div class="social d-flex text-center">
-                                    <a href="#" class="px-2 py-2 mr-md-1 rounded"><span class="ion-logo-facebook mr-2"></span> Facebook</a>
-                                    <a href="#" class="px-2 py-2 ml-md-1 rounded"><span class="ion-logo-twitter mr-2"></span> Twitter</a>
-                                </div>
-                            </div>
+
+                        <div className={styles['group']} style={{ marginTop: '40px' }}>
+                            <span className={styles['p-float-label']}>
+                                {/* <Password
+                                    value={password}
+                                    inputClassName="input"
+                                    style={{ width: '100%' }}
+                                    onChange={e => setPassword(e.target.value)}
+                                    strongRegex={strongPasswordPattern}
+                                    toggleMask
+                                /> */}
+                                <input className={styles['input']} type="text" />
+                                <label htmlFor="pass" style={{ paddingLeft: '20px' }} className={styles['label']}>Password</label>
+                            </span>
+
+                            {/* <ValidationMessage passwordStrengthMessage={showPasswordValidationMessage} /> */}
                         </div>
+
+                        {/* <div className="group" style={{ marginTop: '40px' }}>
+                            <InputSwitch id='check' checked={keepSigned} onChange={e => setKeepSigned(e.target.value)} />
+                            <label htmlFor="check" style={{ color: '#ffffff', marginLeft: '10px' }}>
+                                <b>Keep me Signed in</b>
+                            </label>
+                        </div> */}
+
+                        <div className={styles['group']}>
+                            <input type="submit" className={styles['button']} value="Sign In" />
+                        </div>
+
+                        <div className="hr"></div>
+
+                        <div className={styles['foot-lnk']}><a href="#forgot">Forgot Password?</a></div>
                     </div>
                 </div>
-            </section>
-        </body>
+            </div>
+        </div>
     )
 }
 
